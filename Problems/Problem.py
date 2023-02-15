93ef813b7ae179c0ea4c7515c5e66b5fef40f9d3
EEDK <kde9604@naver.com>
2023-02-15 14:22:00 +0900

    Add BFS PROBLEM

--- Problems/Problem.py
+++ Problems/Problem.py
@@ -1,5 +1,26 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/67256
+# https://school.programmers.co.kr/learn/courses/30/lessons/43165
 
-def solution(numbers, hand):
-    answer = ''
+def solution(numbers, target):
+    answer = 0
+
+    listAnswer = [0]
+    for num in numbers:
+        tmp = []
+        for i in listAnswer:
+            tmp.append(i + num)
+            tmp.append(i - num)
+        print(tmp)
+        listAnswer = tmp
+
+    print(listAnswer)
+    for i in listAnswer:
+        if (target == i):
+            answer += 1
+
     return answer
+
+
+number = [1, 1, 1, 1, 1]
+target = 3
+
+print(solution(number, target))
