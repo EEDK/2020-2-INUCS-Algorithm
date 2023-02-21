0d5eb30c619785942890e41e9866a2f352cb500a
KDE <kde9604@naver.com>
2023-02-21 21:11:29 +0900

    Test sourceTree

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
+if __name__ == "__main__":
 
-def solution(a, b, c):
-
-    sumAmount = a + b + c
-
-    if (sumAmount - max(a, b, c) <= sumAmount / 2):
-        return "Invalid"
-
-    if (a == b and a == c):
-        return "Equilateral"
-    elif (a == b or a == c or b == c):
-        return "Isosceles"
-    else:
-        return "Scalene"
-
-
-while True:
-    a, b, c = map(int, input().split())
-    if (a == 0 and b == 0 and c == 0):
-        break
-
-    print(solution(a, b, c))
+    print("hello world")
