1b9c0be920b12b053f1bc2d6cb947a7924bbf681
KDE <kde9604@naver.com>
2023-02-25 13:44:57 +0900

    .

--- Problems/kakao/kakao2022_blind.py
+++ Problems/kakao/kakao2022_blind.py
@@ -16,6 +16,16 @@ def solution(n, k):
     return answer
 
 
+def isDigit(num):
+    i = 2
+    while i < num:
+        if num % i == 0:
+            return False
+        i += 1
+
+    return True
+
+
 n = 437674
 k = 3
 
