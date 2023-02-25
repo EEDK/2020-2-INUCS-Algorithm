015b778e0f1c6284d594d6adb864b73cfd4f397f
KDE <kde9604@naver.com>
2023-02-25 14:07:05 +0900

    add kakko 2022 solve

--- Problems/kakao/kakao2022_blind.py
+++ Problems/kakao/kakao2022_blind.py
@@ -1,29 +1,64 @@
+"""
+문제 설명
+양의 정수 n이 주어집니다. 이 숫자를 k진수로 바꿨을 때, 변환된 수 안에 아래 조건에 맞는 소수(Prime number)가 몇 개인지 알아보려 합니다.
+
+0P0처럼 소수 양쪽에 0이 있는 경우
+P0처럼 소수 오른쪽에만 0이 있고 왼쪽에는 아무것도 없는 경우
+0P처럼 소수 왼쪽에만 0이 있고 오른쪽에는 아무것도 없는 경우
+P처럼 소수 양쪽에 아무것도 없는 경우
+단, P는 각 자릿수에 0을 포함하지 않는 소수입니다.
+예를 들어, 101은 P가 될 수 없습니다.
+예를 들어, 437674을 3진수로 바꾸면 211020101011입니다. 여기서 찾을 수 있는 조건에 맞는 소수는 왼쪽부터 순서대로 211, 2, 11이 있으며, 총 3개입니다. (211, 2, 11을 k진법으로 보았을 때가 아닌, 10진법으로 보았을 때 소수여야 한다는 점에 주의합니다.) 211은 P0 형태에서 찾을 수 있으며, 2는 0P0에서, 11은 0P에서 찾을 수 있습니다.
+
+정수 n과 k가 매개변수로 주어집니다. n을 k진수로 바꿨을 때, 변환된 수 안에서 찾을 수 있는 위 조건에 맞는 소수의 개수를 return 하도록 solution 함수를 완성해 주세요.
+"""
+
+import math
+
+
 def solution(n, k):
-    answer = -1
+    answer = 0
+    kDigit = convert(n, k)
+    checkList = list()
+    tmpList = ''
+
+    for i in range(len(kDigit)):
+        if kDigit[i] == '0' and tmpList != '':
+            checkList.append(tmpList)
+            tmpList = ''
+        else:
+            tmpList += kDigit[i]
+
+    checkList.append(tmpList)
+    for s in checkList:
+        if isDigit(int(s)):
+            answer += 1
+
+    return answer
+
+
+def isDigit(x):
+    if x <= 1:
+        return False
+    for i in range(2, int(math.sqrt(x) + 1)):
+        if x % i == 0:
+            return False
+    return True
+
+
+def convert(n, dig):
 
     stack = list()
     kDigit = ""
 
     while n:
-        stack += str(n % k)
-        n //= 3
+        stack += str(n % dig)
+        n //= dig
 
     while stack:
         kDigit += stack.pop()
 
-    print(kDigit)
-
-    return answer
-
-
-def isDigit(num):
-    i = 2
-    while i < num:
-        if num % i == 0:
-            return False
-        i += 1
-
-    return True
+    return kDigit
 
 
 n = 437674
