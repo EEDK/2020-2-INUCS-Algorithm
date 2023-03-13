a7bfedb58fd30a07ca3ee179df64903653c25b37
KDE <kde9604@naver.com>
2023-03-13 22:19:58 +0900

    1

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
-def solution(board, moves):
-    answer = 0
+def solution(msg):
+    dict = {}
 
-    lst = [0]
+    idx = 1
+    for i in range(65, 91):
+        dict[idx] = chr(i)
+        idx += 1
 
-    for target in moves:
-        for j in range(len(board)):
-            if board[j][target-1] != 0:
-                lst.append(board[j][target-1])
-                board[j][target-1] = 0
-
-                tmp1 = lst.pop()
-                tmp2 = lst.pop()
-                if tmp1 == tmp2:
-                    answer += 2
-                else:
-                    lst.append(tmp2)
-                    lst.append(tmp1)
-
-                break
-            elif board[j][target-1] == 0:
-                continue
-
+    answer = []
     return answer
 
 
-print(solution([[0, 0, 0, 0, 0], [0, 0, 1, 0, 3], [0, 2, 5, 0, 1], [
-      4, 2, 4, 4, 2], [3, 5, 1, 3, 1]], [1, 5, 3, 5, 1, 2, 1, 4]))
+print(solution('KAKAO'))
